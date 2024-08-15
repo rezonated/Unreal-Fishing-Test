37775323731f974450d34ed001ca9e42ade84afc
Vanan Andreas <rezonated@gmail.com>
2024-08-15 09:51:23 +0700

    Initial blank project setup

---
+++ Source/StairwayFishingGame/StairwayFishingGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StairwayFishingGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STAIRWAYFISHINGGAME_API AStairwayFishingGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
