d4b19e19aad12ffb7220a16821c7ee5c6c9c5e9c
Vanan Andreas <rezonated@gmail.com>
2024-08-19 13:25:16 +0700

    Removed clang format from root Reorganize class structure, now there's class button in FishingFeature module that inherits from emitting message button class Tested package Fix screen transition code Do cleanup of async listeners Changed state from enum to gameplay tag

---
+++ Source/FishingFeature/Private/UserWidget/Button/MessageEmittingButton/UserWidgetMessageEmittingButton_RestartButton.cpp
@@ -0,0 +1,4 @@
+﻿// Vanan Andreas - 2024
+
+
+#include "UserWidget/Button/MessageEmittingButton/UserWidgetMessageEmittingButton_RestartButton.h"
---
+++ Source/FishingFeature/Public/UserWidget/Button/MessageEmittingButton/UserWidgetMessageEmittingButton_RestartButton.h
@@ -0,0 +1,16 @@
+﻿// Vanan Andreas - 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StairwayFishingGameUI/Public/UserWidget/Button/UserWidgetButton_MessageEmittingButton.h"
+#include "UserWidgetMessageEmittingButton_RestartButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FISHINGFEATURE_API UUserWidgetMessageEmittingButton_RestartButton : public UUserWidgetButton_MessageEmittingButton
+{
+	GENERATED_BODY()
+};
